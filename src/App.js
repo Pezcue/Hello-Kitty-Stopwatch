5c057a0dc74335781c417f5964164c071162336b
Pezcue <peshugadeposho@hotmail.com>
2024-07-25 13:59:19 -0400

    Details

--- src/App.js
+++ src/App.js
@@ -18,32 +18,43 @@ function App() {
    }, [running]);
 
   return (
-    <div className="bg-orange-50 flex flex-col items-center justify-center min-h-screen border-8 border-black">
-    <h1 className='text-lg font-semibold'>Hello Kitty</h1>
-    <h1 className='text-lg font-semibold'>Stopwatch</h1>
+    <div style={{
+      backgroundImage: "url('./images/hellokitty.png')",
+      backgroundPosition: 'center',
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+
+      width: '45%',// Adjust the height to your needs
+      height: '450px',    // Adjust the width to your needs
+    }}
+     className="bg-orange-50 flex flex-col items-center justify-center min-h-screen border-8 border-black">
+    <h1 className='text-7xl font-semibold'>The Hello Kitty</h1>
+    <h1 className='text-red-700 text-5xl font-semibold m-6'>Stopwatch</h1>
     <div style={{
           backgroundImage: "url('./images/hellokitty.png')",
-          backgroundSize: 'cover',
           backgroundPosition: 'center',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+  
           width: '45%',// Adjust the height to your needs
-          height: '350px',    // Adjust the width to your needs
+          height: '450px',    // Adjust the width to your needs
         }}
-        className="text-xl font-semibold py-4 border-2 border-rose-600">
+        className="text-5xl flex items-center justify-center text-xl font-semibold py-4">
       <span>{("0" + Math.floor((time/60000) % 60)).slice(-2)}:</span>
       <span>{("0" + Math.floor((time/1000) % 60)).slice(-2)}:</span>
       <span>{("0" + ((time/10) % 100)).slice(-2)}</span>
     </div>
-    <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+    <div className='text-base w-1/3 max-w-sm flex flex-row justify-evenly'>
       {running ? (
         <button 
-        className='border rounded-lg py-1 px-2.5'
+        className='bg-blue-600 text-white border rounded-lg py-1 px-4'
          onClick={()=>{setRunning(false)}}
          >
           Stop
         </button>
     ) : (
       <button
-      className='bg-red-600 border rounded-lg py-1 px-3'
+      className='bg-red-600 text-white border rounded-lg py-1 px-4'
        onClick={()=>{setRunning(true)}}
        >
         Start
@@ -51,7 +62,7 @@ function App() {
     )
     }
       <button
-      className='bg-red-600 border rounded-lg py-1 px-2'
+      className='bg-red-600 text-white border rounded-lg py-1 px-4 '
        onClick={()=>{setTime(0) }
        }
        >
